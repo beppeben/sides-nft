0503a965abf0d06c74c45b615ca3affdd511f467
giuseppe <beppeben2030@gmail.com>
2023-02-25 13:18:36 +0100

    escaped special characters

--- frontend/pages/about.js
+++ frontend/pages/about.js
@@ -11,13 +11,13 @@ export default function About() {
       <div className="container">
         <div className="center-text">
           <p>SidesNFT is a prototype implementation of the Adaptive NFT Collection standard.</p>
-          <p>Adaptive collections are collections where the supply and rarity distribution are a function of the users' preferences and interactions, evolving over time.</p>
+          <p>Adaptive collections are collections where the supply and rarity distribution are a function of the users&#39; preferences and interactions, evolving over time.</p>
           <p>Rarity is organized into Levels.</p>
           <ul>
             <li>Level-0 NFTs are mintable by everyone (possibly with a date and supply limit).</li>
-            <li>Higher levels are mintable by burning ("evolving") a certain number (here, 3) of NFTs of the previous level.</li>        
+            <li>Higher levels are mintable by burning (&#34;evolving&#34;) a certain number (here, 3) of NFTs of the previous level.</li>        
             <li>One can also downgrade an NFT into a certain number (here, 2) of NFTs of the previous level.</li>
-            <li>SidesNFT assets get assigned a shape with a number of sides corresponding to their level, and with a random color. This behaviour can be customized and extended in various ways in the smart contract, for example using assignments based on user's holdings, possibly corresponding to IRL challenges.</li>
+            <li>SidesNFT assets get assigned a shape with a number of sides corresponding to their level, and with a random color. This behaviour can be customized and extended in various ways in the smart contract, for example using assignments based on user&#39;s holdings, possibly corresponding to IRL challenges.</li>
           </ul>
           <p>Once an exchange system is put in place (possibly even an IRL one), people can start hunting for the highest level assets, either for rewards or simple glory.</p>
         </div>
